33083976b2138aa42fe19ba1e2d9da8a7bf51fb1
Arnav09Mangla <88844265+Arnav09Mangla@users.noreply.github.com>
2023-02-22 10:23:12 +0530

    Add files via upload

---
+++ Project 102/Move_File.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+from_dir = "C:/users/ADMIN/Downloads"
+to_dir = "C:/WhiteHatJR/downloadimages"
+list_of_file = os.listdir(from_dir)
+print(list_of_files)
+if os.path.exists(path2):
+    print("Moving " + file_name + ".....")
+
+
+    shutil.move(path1, path3)
+
+    else:
+      os.makedirs(path2)
+      print("Moving " + file_name + ".....")  
+      shutil.move(path1, path3)
+
+    
